3212d75026086d654d0596ea7cfabfb975c7d98d
qrvd <36740654+qrvd@users.noreply.github.com>
2018-04-06 06:46:02 +0200

    finish section on numbers

--- doubleme.hs
+++ doubleme.hs
@@ -1,2 +1,10 @@
 doubleMe n = n + n
-doubleUs x y = x*2 + y*2
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+                      then x
+                      else x * 2
+
+doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+
+mari'O = "It's a-me, Mari'o!"
